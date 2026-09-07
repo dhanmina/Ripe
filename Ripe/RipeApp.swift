4c6a63858e8f6c0174f05bbdb0b84544f796bd94
dhanmina <dhanmina15@gmail.com>
2026-09-07 16:13:27 +0800

    chore: narrow Ripe to a macOS-only menu bar app scaffold

---
+++ Ripe/RipeApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct RipeApp: App {
+    var body: some Scene {
+        MenuBarExtra("Ripe", systemImage: "leaf.fill") {
+            Text("Ripe")
+                .padding()
+        }
+    }
+}
